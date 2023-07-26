92b6e860ddb2fbed86e2b0573bc8550c57ab7eb2
GodVik53 <137499699+GodVik53@users.noreply.github.com>
2023-07-26 10:40:24 +0300

    Create Task_50.cs
    Решили задачу №50

---
+++ Task_50.cs
@@ -0,0 +1,54 @@
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+
+
+
+
+int[,] nums = new int[3, 4];
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        nums[i, j] = new Random().Next(0, 10);
+    }
+}
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        Console.Write(nums[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+int her=0;
+Console.Write("Введите число для поиска: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        if (nums[i, j] == m) {
+
+        Console.Write ($"Указанное число находится в ячейке с адресом:  {i}, {j}");
+        Console.WriteLine();
+        her++;
+
+    }
+
+    
+}
+
+}
+if (her ==0)
+Console.WriteLine("Указанного числа нет в массиве");
